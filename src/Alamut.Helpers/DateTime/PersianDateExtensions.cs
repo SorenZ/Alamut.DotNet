6066774b3dbac852158314a1c4fb40ea69b757b7
Soren Z.Vand <sunrise.freedom@gmail.com>
2016-10-03 14:17:05 +0330

    -- helpers code clean-up.

--- src/Alamut.Helpers/DateTime/PersianDateExtensions.cs
+++ src/Alamut.Helpers/DateTime/PersianDateExtensions.cs
@@ -2,28 +2,11 @@
 
 namespace Alamut.Helpers.DateTime
 {
+    /// <summary>
+    /// provide localized persian date-time extensions
+    /// </summary>
     public static class PersianDateExtensions
     {
-        public static string GetPersianMonthName(int month)
-        {
-            switch (month)
-            {
-                case 1: { return "فروردین";}
-                case 2: { return "اردیبهشت";}
-                case 3: { return "خرداد"; }
-                case 4: { return "تیر"; }
-                case 5: { return "مرداد"; }
-                case 6: { return "شهریور"; }
-                case 7: { return "مهر"; }
-                case 8: { return "آبان"; }
-                case 9: { return "آذر"; }
-                case 10: { return "دی"; }
-                case 11: { return "بهمن"; }
-                case 12: { return "اسفند"; }
-                default: { return "نا مشخص"; }
-            }
-        }
-
         public static string GetPersianDateString(this System.DateTime date)
         {
             var p = new System.Globalization.PersianCalendar();
@@ -34,7 +17,7 @@ namespace Alamut.Helpers.DateTime
 
             var monthName = GetPersianMonthName(month);
 
-            return string.Format("{0} {1} {2}", day, monthName, year);
+            return $"{day} {monthName} {year}";
         }
 
         public static string GetPersianDateStringSlashed(this System.DateTime date)
@@ -59,21 +42,43 @@ namespace Alamut.Helpers.DateTime
 
         public static string GetFullPersianDateTime(this System.DateTime date)
         {
+            //var localDate = date.ToLocalTime();
             var time = date.ToString("HH:mm");
             var persianDateString = GetPersianDateString(date);
-            var day = "";
+            string day;
+
             switch (date.DayOfWeek)
             {
-                case DayOfWeek.Saturday: day = "شنبه"; break;
-                case DayOfWeek.Sunday: day = "يکشنبه"; break;
-                case DayOfWeek.Monday: day = "دوشنبه"; break;
-                case DayOfWeek.Tuesday: day = "سه‏ شنبه"; break;
-                case DayOfWeek.Wednesday: day = "چهارشنبه"; break;
-                case DayOfWeek.Thursday: day = "پنجشنبه"; break;
-                case DayOfWeek.Friday: day = "جمعه"; break;
-                default: day = ""; break;
+                case DayOfWeek.Saturday: day = @"شنبه"; break;
+                case DayOfWeek.Sunday: day = @"يکشنبه"; break;
+                case DayOfWeek.Monday: day = @"دوشنبه"; break;
+                case DayOfWeek.Tuesday: day = @"سه‏ شنبه"; break;
+                case DayOfWeek.Wednesday: day = @"چهارشنبه"; break;
+                case DayOfWeek.Thursday: day = @"پنجشنبه"; break;
+                case DayOfWeek.Friday: day = @"جمعه"; break;
+                default: day = string.Empty; break;
             }
-            return string.Format("{0} ، {1} ساعت : {2}", day, persianDateString, time);
+            return $"{day} ، {persianDateString} ساعت : {time}";
+        }
+
+        public static string GetPersianMonthName(int month)
+        {
+            switch (month)
+            {
+                case 1: { return "فروردین"; }
+                case 2: { return "اردیبهشت"; }
+                case 3: { return "خرداد"; }
+                case 4: { return "تیر"; }
+                case 5: { return "مرداد"; }
+                case 6: { return "شهریور"; }
+                case 7: { return "مهر"; }
+                case 8: { return "آبان"; }
+                case 9: { return "آذر"; }
+                case 10: { return "دی"; }
+                case 11: { return "بهمن"; }
+                case 12: { return "اسفند"; }
+                default: { return "نا مشخص"; }
+            }
         }
     }
 }
