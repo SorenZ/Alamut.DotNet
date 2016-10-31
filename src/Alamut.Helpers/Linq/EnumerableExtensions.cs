7b9931d0b5fd7a12596d9a987fa9980202fb28d5
Soren Z.Vand <sunrise.freedom@gmail.com>
2016-10-31 11:58:50 +0330

    -- add Partition method in EnumerableExtensions

--- src/Alamut.Helpers/Linq/EnumerableExtensions.cs
+++ src/Alamut.Helpers/Linq/EnumerableExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Dynamic;
 using System.Linq;
 
@@ -58,6 +59,41 @@ namespace Alamut.Helpers.Linq
                     collection.Remove(element);
                     i--;
                 }
+            }
+        }
+
+        /// <summary>
+        /// Split a collection into n parts
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="size"></param>
+        /// <remarks>http://stackoverflow.com/a/438208/428061</remarks>
+        /// <returns></returns>
+        public static IEnumerable<IEnumerable<T>> Partition<T>
+            (this IEnumerable<T> source, int size)
+        {
+            T[] array = null;
+            int count = 0;
+            foreach (T item in source)
+            {
+                if (array == null)
+                {
+                    array = new T[size];
+                }
+                array[count] = item;
+                count++;
+                if (count == size)
+                {
+                    yield return new ReadOnlyCollection<T>(array);
+                    array = null;
+                    count = 0;
+                }
+            }
+            if (array != null)
+            {
+                Array.Resize(ref array, count);
+                yield return new ReadOnlyCollection<T>(array);
             }
         }
 
