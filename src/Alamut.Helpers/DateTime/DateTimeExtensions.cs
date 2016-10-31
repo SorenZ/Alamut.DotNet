c532717d6a41a9e8ae47a8579e0fff0527261677
Soren Z.Vand <sunrise.freedom@gmail.com>
2016-10-31 11:54:01 +0330

    Add Datetime-Extensions.

---
+++ src/Alamut.Helpers/DateTime/DateTimeExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Alamut.Helpers.DateTime
+{
+    public static class DateTimeExtensions 
+    {
+        /// <summary>
+        /// get the datetime of the start of the week
+        /// </summary>
+        /// <param name="dt"></param>
+        /// <param name="startOfWeek"></param>
+        /// <returns></returns>
+        /// <example>
+        /// DateTime dt = DateTime.Now.StartOfWeek(DayOfWeek.Monday);
+        /// DateTime dt = DateTime.Now.StartOfWeek(DayOfWeek.Sunday);
+        /// </example>
+        /// <remarks>http://stackoverflow.com/a/38064/428061</remarks>
+        public static System.DateTime StartOfWeek(this System.DateTime dt, DayOfWeek startOfWeek)
+        {
+            var diff = dt.DayOfWeek - startOfWeek;
+            if (diff < 0)
+                diff += 7;
+
+            return dt.AddDays(-1 * diff).Date;
+        }
+
+        /// <summary>
+        /// get the datetime of the start of the month
+        /// </summary>
+        /// <param name="dt"></param>
+        /// <returns></returns>
+        /// <remarks>http://stackoverflow.com/a/5002582/428061</remarks>
+        public static System.DateTime StartOfMonth(this System.DateTime dt)
+        {
+            var now = System.DateTime.Now;
+            return new System.DateTime(now.Year, now.Month, 1);
+        }
+
+        /// <summary>
+        /// get datetime of the start of the year
+        /// </summary>
+        /// <param name="dt"></param>
+        /// <returns></returns>
+        public static System.DateTime StartOfYear(this System.DateTime dt)
+        {
+            var now = System.DateTime.Now;
+            return new System.DateTime(now.Year, 1, 1);
+        }
+    }
+}
