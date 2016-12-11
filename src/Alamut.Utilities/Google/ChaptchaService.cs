cc654ac32704f4c560fd3a8de38f2d09ee772a40
Soren Z.Vand <sunrise.freedom@gmail.com>
2016-12-11 14:40:31 +0330

    -- utility => add Google Captcha component -- test => add web test in order to real test some components.

---
+++ src/Alamut.Utilities/Google/ChaptchaService.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Alamut.Data.Structure;
+using Newtonsoft.Json;
+
+namespace Alamut.Utilities.Google
+{
+    public class CaptchaService
+    {
+        private readonly GoogleCaptchaConfig _chaptchaConfig;
+
+        public CaptchaService(GoogleCaptchaConfig chaptchaConfig)
+        {
+            _chaptchaConfig = chaptchaConfig;
+        }
+
+        public async Task<ServiceResult> ValidateGoogleChaptcha(string rechaptchaKey)
+        {
+
+            using (var client = new HttpClient())
+            {
+                var values = new Dictionary<string, string>
+                {
+                    {"secret", _chaptchaConfig.SecretKey},
+                    {"response", rechaptchaKey}
+                };
+
+                try
+                {
+                    var response = await client.PostAsync(_chaptchaConfig.ApiUrl, new FormUrlEncodedContent(values));
+                    //var result = await response.Content.ReadAsAsync<GoogelSiteVerifyResponse>();
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var json = await response.Content.ReadAsStringAsync();
+                        var result = JsonConvert.DeserializeObject<GoogelSiteVerifyResponse>(json);
+                        return result.success ? ServiceResult.Okay() : ServiceResult.Error();
+                    }
+
+                    return ServiceResult.Error(response.ReasonPhrase, (int) response.StatusCode);
+                }
+                catch (Exception ex)
+                {
+                    return ServiceResult.Exception(ex);
+                }
+            }
+        }
+
+        class GoogelSiteVerifyResponse
+        {
+            public bool success { get; set; }
+            public string challenge_ts { get; set; }
+            public string hostname { get; set; }
+            //public string error-codes { get; set; }
+        }
+    }
+
+    public class GoogleCaptchaConfig
+    {
+        public string SecretKey { get; set; }
+        public string SiteKey { get; set; }
+        public string ApiUrl { get; set; } = "https://www.google.com/recaptcha/api/siteverify";
+    }
+}
