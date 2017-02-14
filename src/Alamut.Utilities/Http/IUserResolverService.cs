1cd9e1c3e35b01a343bc9e68b3b241e8de3fddd1
Soren Zand <sunrise.freedom@gmail.com>
2017-02-14 15:12:55 +0330

    -- create user-resolver inteface (to use in service) -- create user-resolver-by-HttpContext concrete class.

---
+++ src/Alamut.Utilities/Http/IUserResolverService.cs
@@ -0,0 +1,31 @@
+﻿namespace Alamut.Utilities.Http
+{
+    public interface IUserResolverService
+    {
+        /// <summary>
+        /// gets current user Username
+        /// </summary>
+        string UserName { get; }
+
+        /// <summary>
+        /// gets current user full name (first name + family)
+        /// </summary>
+        string Name { get; }
+
+        /// <summary>
+        /// gets current user friendly name (fore name)
+        /// if it doesn't represented return userName
+        /// </summary>
+        string GivenName { get; }
+
+        /// <summary>
+        /// gets current user UserId
+        /// </summary>
+        string UserId { get; }
+
+        /// <summary>
+        /// gets current user Ip Address
+        /// </summary>
+        string UserIpAddress { get; }
+    }
+}
---
+++ src/Alamut.Utilities/Http/UserResolverServiceByHttpContext.cs
@@ -0,0 +1,31 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+
+namespace Alamut.Utilities.Http
+{
+    /// <summary>
+    /// provide some information about Identity User throughout  domain services
+    /// by the help of HttpContextAccessor
+    /// </summary>
+    /// <remarks>
+    /// it can be used when HTTP Context is available
+    /// </remarks>
+    public class UserResolverServiceByHttpContext : IUserResolverService
+    {
+        private readonly IHttpContextAccessor _context;
+
+        public UserResolverServiceByHttpContext(IHttpContextAccessor context)
+        {
+            _context = context;
+        }
+
+        public string UserName => _context.HttpContext.User?.Identity?.Name;
+        public string Name => _context.HttpContext.User.FindFirst(ClaimTypes.Name)?.Value;
+        public string GivenName => _context.HttpContext.User.FindFirst(ClaimTypes.GivenName)?.Value;
+        public string UserId => _context.HttpContext.User?.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        public string UserIpAddress => _context.HttpContext
+            .Features.Get<IHttpConnectionFeature>()?
+            .RemoteIpAddress.ToString();
+    }
+}
