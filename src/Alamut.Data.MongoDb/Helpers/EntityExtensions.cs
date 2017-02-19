38a532783aa3bd290675dc8f0036111461c47ffb
Soren Zand <sunrise.freedom@gmail.com>
2017-02-19 13:38:01 +0330

    - code clean-ups. - add tasks and launch (json).

--- src/Alamut.Data.MongoDb/Helpers/EntityExtensions.cs
+++ src/Alamut.Data.MongoDb/Helpers/EntityExtensions.cs
@@ -1,7 +1,4 @@
-﻿using System.Text;
-using System.Threading.Tasks;
-
-namespace Alamut.Data.MongoDb.Helpers
+﻿namespace Alamut.Data.MongoDb.Helpers
 {
     public static class EntityExtensions
     {
--- src/Alamut.Data/Repository/IQueryRepository.cs
+++ src/Alamut.Data/Repository/IQueryRepository.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using Alamut.Data.Entity;
-using Alamut.Data.NoSql;
 using Alamut.Data.Paging;
 
 namespace Alamut.Data.Repository
