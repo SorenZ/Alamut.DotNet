72e6cdce4edf30fbbcc8c10e50df23c6f78a2f6d
Soren Zand <sunrise.freedom@gmail.com>
2017-05-30 15:24:23 +0430

    add Alamut.Utilities.AspNet projects and some utilities.

---
+++ src/Alamut.Utilities.AspNet/Configuration/ServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+// https://www.strathweb.com/2016/09/strongly-typed-configuration-in-asp-net-core-without-ioptionst/
+namespace Alamut.Utilities.AspNet.Configuration
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static TConfig ConfigurePoco<TConfig>(this IServiceCollection services, IConfiguration configuration) where TConfig : class, new()
+        {
+            if (services == null) throw new ArgumentNullException(nameof(services));
+            if (configuration == null) throw new ArgumentNullException(nameof(configuration));
+
+            var config = new TConfig();
+            configuration.Bind(config);
+            services.AddSingleton(config);
+            return config;
+        }
+    }
+}
