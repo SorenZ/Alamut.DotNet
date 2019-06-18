9a7386a7f08a41de3e9a26e6867b930c90f204de
Hamed Zohrevand <H.Zohrevand@mobinnet.net>
2019-06-18 09:03:47 +0430

    Alamut.Data.Test project added to test Alamut.Data library

---
+++ test/Alamut.Data.Test/ServiceResultTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xunit;
+
+namespace Alamut.Data.Test
+{
+    public class ServiceResultTest
+    {
+        [Fact]
+        public void ServiceResult_Equals_In_Same_Object()
+        {
+                
+        }
+
+        class Fool
+        {
+            public string Bar { get; set; }
+        }
+    }
+}
