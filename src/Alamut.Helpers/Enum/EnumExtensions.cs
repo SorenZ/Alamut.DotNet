97d27c3c010f919df566b3edab7a30c234a5ac35
Hamed ZVand <sunrise.freedom@gmail.com>
2019-08-14 08:39:39 +0430

    implement GetAttributeOfType in EnumExtenstions

--- src/Alamut.Helpers/Enum/EnumExtensions.cs
+++ src/Alamut.Helpers/Enum/EnumExtensions.cs
@@ -39,14 +39,14 @@ namespace Alamut.Helpers.Enum
         /// <param name="enumVal">The enum value</param>
         /// <returns>The attribute of type T that exists on the enum value</returns>
         /// <example>string desc = myEnumVariable.GetAttributeOfType<DescriptionAttribute>().Description;</example>
-        //public static T GetAttributeOfType<T>(this System.Enum enumVal) where T : System.Attribute
-        //{
-        //    var type = enumVal.GetType();
-        //    var memInfo = type.GetMember(enumVal.ToString());
-        //    var attributes = memInfo[0].GetCustomAttributes(typeof(T), false);
-        //    return (attributes.Lenght > 0)
-        //        ? (T)attributes[0]
-        //        : null;
-        //}
+        public static T GetAttributeOfType<T>(this System.Enum enumVal) where T : System.Attribute
+        {
+            var type = enumVal.GetType();
+            var memInfo = type.GetMember(enumVal.ToString());
+            var attributes = memInfo[0].GetCustomAttributes(typeof(T), false);
+            return (attributes.Length > 0)
+                ? (T)attributes[0]
+                : null;
+        }
     }
 }
