98a62847dcf2adc9b0ece3e549214580906b1e78
Hamed ZVand <sunrise.freedom@gmail.com>
2019-08-17 10:37:22 +0430

    remove useless old folders

--- nuget/alamut-pack.bat
+++
@@ -1,6 +0,0 @@
-REM dotnet pack ..\src\Alamut.Core\project.json --output ..\packages
-dotnet pack ..\src\Alamut.Data --output ..\..\packages
-dotnet pack ..\src\Alamut.Data.MongoDb\Alamut.Data.MongoDb.csproj --output ..\packages
-dotnet pack ..\src\Alamut.Helpers\project.json --output ..\packages
-dotnet pack ..\src\Alamut.Service\project.json --output ..\packages
-dotnet pack ..\src\Alamut.Utilities\Alamut.Utilities.csproj --output ..\..\packages
--- nuget/alamut-publish.bat
+++
@@ -1,9 +0,0 @@
-REM nuget.exe push -Source http://package.samsystems.ir/nuget -ApiKey poshtkohi ..\packages\Alamut.Core.2.1.0.nupkg
-nuget.exe push -Source http://package.samsystems.ir/nuget -ApiKey poshtkohi ..\packages\Alamut.Data.3.0.1.nupkg
-nuget.exe push -Source http://package.samsystems.ir/nuget -ApiKey poshtkohi ..\packages\Alamut.Data.MongoDb.3.0.2.nupkg
-nuget.exe push -Source http://package.samsystems.ir/nuget -ApiKey poshtkohi ..\packages\Alamut.Helpers.3.0.1.nupkg
-nuget.exe push -Source http://package.samsystems.ir/nuget -ApiKey poshtkohi ..\packages\Alamut.Service.3.0.2.nupkg
-nuget.exe push -Source http://package.samsystems.ir/nuget -ApiKey poshtkohi ..\packages\Alamut.Utilities.3.0.1.nupkg
-
-
-nuget.exe push -Source http://package.samsystems.ir/nuget -ApiKey poshtkohi ..\packages\Alamut.Data.3.0.1.nupkg
