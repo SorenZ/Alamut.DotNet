d947a832813fee67c954bc4cd4f1a7f6fff30712
Hamed ZVand <sunrise.freedom@gmail.com>
2019-12-21 10:19:08 +0330

    added IdGenerator

---
+++ src/Alamut.Helpers/DomainDriven/IdGenerator.cs
@@ -0,0 +1,28 @@
+using MongoDB.Bson;
+
+namespace Alamut.Helpers.DomainDriven
+{
+    /// <summary>
+    /// responsible for generate unique Id
+    /// </summary>
+    public class IdGenerator
+    {
+        /// <summary>
+        /// provides a unique id based on BSON object Id
+        /// </summary>
+        /// <returns></returns>
+        public string GetNewId()
+        {
+            return ObjectId.GenerateNewId().ToString();
+        }
+
+        /// <summary>
+        /// provides a new unique ObjectId 
+        /// </summary>
+        /// <returns></returns>
+        public ObjectId GetObjectId()
+        {
+            return ObjectId.GenerateNewId();
+        }
+    }
+}
